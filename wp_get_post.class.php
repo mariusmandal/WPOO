a4902c9544cea2888c548a28409675bfe183094f
Marius Mandal <mariusmandal@gmail.com>
2013-11-09 17:37:59 +0100

    Add facebook shares-count

--- wp_get_post.class.php
+++ wp_get_post.class.php
@@ -17,6 +17,9 @@ class wp_get_post {
 		$this->_content($post);
 		$this->_url($post);
 		$this->_category($post);
+		
+		$this->facebook = new stdClass;
+		$this->facebook->shares = 0;
 	}
 	
 	private function _category(&$post) {
@@ -65,8 +68,30 @@ class wp_get_post {
 		}
 		$this->image->ID = $image;
 		$this->image->url = wp_get_attachment_url($this->image->ID);
-		$this->image->src = wp_get_attachment_image_src($this->image->ID);
-#		$post->image->meta = wp_get_attachment_metadata($post->image->ID);
+		$source_data = wp_get_attachment_image_src($this->image->ID);
+		$this->image->src = $source_data[0];
+		$this->image->width = $source_data[1];
+		$this->image->height = $source_data[2];
+		#		$post->image->meta = wp_get_attachment_metadata($post->image->ID);
+	}
+	
+	public function facebook_value() {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, 'https://graph.facebook.com/'.$this->url.'?method=GET&format=json&suppress_http_code=1&');
+		curl_setopt($ch, CURLOPT_REFERER, $_SERVER['PHP_SELF']);
+		curl_setopt($ch, CURLOPT_USERAGENT, "WPOO");
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 1);
+		
+		$result = curl_exec( $ch );
+		$result = json_decode($result);
+		
+		if( is_object( $result ) ) {
+			$this->facebook->shares = $result->shares;
+		} else {
+			$this->facebook->shares = false;
+		}
 	}
 }
 ?>
