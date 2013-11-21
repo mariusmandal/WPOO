fb73ca099cac1868410b7e9441968861be10360c
Marius Mandal <mariusmandal@gmail.com>
2013-11-21 17:41:17 +0100

    Add gravatar support

--- WPOO/Comment.php
+++ WPOO/Comment.php
@@ -9,9 +9,20 @@ Author URI: http://www.mariusmandal.no
 */
 class WPOO_Comment {
 	public function __construct( $comment_data ) {
+		$this->_load_data( $comment_data );
+		$this->_load_gravatar();
+	}
+	
+	private function _load_data( $comment_data ) {
 		foreach( $comment_data as $key => $val) {
 			$new_key = str_replace('comment_', '', $key);
 			$this->$new_key = $val;
 		}
+	}
+	
+	private function _load_gravatar() {
+		$this->avatar = get_avatar( $this->author_email, 96, 'mm' );
+		preg_match('/< *img[^>]*src *= *["\']?([^"\']*)/i', $this->avatar, $matches);
+		$this->avatar_url = $matches[1];
 	}
 }
