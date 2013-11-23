e98f1160ae90be285cebdef0718699000cec14e5
Daniel Skogstad <daniel.skogstad@gmail.com>
2013-11-23 15:26:09 +0100

    updated author class

--- WPOO/Author.php
+++ WPOO/Author.php
@@ -12,24 +12,28 @@ class WPOO_Author {
     public $id;
     public $login;
     public $nicename;
-    public $email;
+    public #user_email;
     public $url;
+    public $image;
+    public $company_name;
     public $registered;
     public $display_name;
     public $firstname;
     public $lastname;
     public $nickname;
     public $description;
-    public $facebook;
+    public facebook_url;
     public $title;
     public $link;
 
     public function __construct($wpUser)
     {
         $this->id = $wpUser->ID;
+        $this->image = $wpUser->image;
+        $this->company_name = $wpUser->company_name;
         $this->login = $wpUser->user_login;
         $this->nicename = $wpUser->nicename;
-        $this->email = $wpUser->email;
+        $this->email = $wpUser->user_email;
         $this->url = $wpUser->url;
         $this->registered = $wpUser->registered;
         $this->display_name = $wpUser->display_name;
@@ -37,7 +41,7 @@ class WPOO_Author {
         $this->lastname = $wpUser->lastname;
         $this->nickname = $wpUser->nickname;
         $this->description = $wpUser->description;
-        $this->facebook = get_the_author_meta( 'facebook', $this->id );
+        $this->facebook_url = get_the_author_meta( 'facebook', $this->id );
         $this->title = get_the_author_meta( 'title', $this->id );
         $this->link = get_author_posts_url( $this->id, $this->nicename );
     }
