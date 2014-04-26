c59fdc6923e2c4f055da85d38a7e5c1dbd82e711
Marius Mandal <mariusmandal@gmail.com>
2014-04-26 22:21:31 +0200

    debug

--- WPOO/Post.php
+++ WPOO/Post.php
@@ -184,10 +184,10 @@ class WPOO_Post {
 			return;
 		}
 		if( $_SERVER['REMOTE_ADDR'] == '195.204.59.122' ) {
-			echo 'Beregn bilde!';
+			echo '<h2>Beregn bilde!</h2><pre>';
 			var_dump( $image );
-			
-			var_dump( wp_get_attachment_url( $image ) );
+			var_dump( wp_get_attachment_image_src( $image ) );
+			echo '</pre>';
 		}
 
 		$this->image->ID = $image;
