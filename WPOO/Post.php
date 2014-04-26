0752b8f7ea7b7544a78b1355b7a661cd5c44d648
Marius Mandal <mariusmandal@gmail.com>
2014-04-26 21:38:53 +0200

    image id from url

--- WPOO/Post.php
+++ WPOO/Post.php
@@ -160,6 +160,10 @@ class WPOO_Post {
 			$image = str_replace('-150x150', '', $image );
 
 			if(!empty($image)) {
+				$image_id = url_to_postid( $image );
+				if( $_SERVER['195.204.59.122'] ) {
+					var_dump( $image_id );
+				}
 				$this->image->ID = 1;
 				$this->image->url = $image;
 				$data = @getimagesize( $image );
